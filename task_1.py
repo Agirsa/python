19cef29fd755b170cdd1e084072823dd79881e3e
Agirsa <79326050+Agirsa@users.noreply.github.com>
2021-03-05 19:22:19 +0300

    Test

---
+++ task_1.py
@@ -0,0 +1,8 @@
+def num_translate(num):
+    print(numbers.get(num))
+
+
+numbers = {"one": "один", "two": "два", "three": "три", "four": "четыре", "five": "пять", "six": "шесть",
+           "seven": "семь", "eight": "восемь", "nine": "девять", "ten": "десять"}
+number = input("Напишите число на английском: ")
+num_translate(number)
