f28764a0b1452ec5afd8ef49f4e5de445dcdb203
Agirsa <79326050+Agirsa@users.noreply.github.com>
2021-03-05 19:23:07 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,11 @@
+def num_translate(num):
+    if num[0].isupper():
+        print(numbers.get(num.lower()).capitalize())
+    else:
+        print(numbers.get(num))
+
+
+numbers = {"one": "один", "two": "два", "three": "три", "four": "четыре", "five": "пять", "six": "шесть",
+           "seven": "семь", "eight": "восемь", "nine": "девять", "ten": "десять"}
+number = input("Напишите число на английском: ")
+num_translate(number)
